70fb13d0d7416d10d8b3e9c2b2f06c7b7848cf4a
earthchild316 <earthchild316@gmail.com>
2020-10-28 16:05:58 +0000

    removed queries file

--- code/queries.sql
+++
@@ -1,4 +0,0 @@
-SELECT students.name, courses.title, bookings.date_booked
-FROM students
-INNER JOIN bookings ON bookings.student_id = students.id
-INNER JOIN courses ON courses.id = bookings.course_id;
